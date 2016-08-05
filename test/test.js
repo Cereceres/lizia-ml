640d2cc91dbbe261001df9c067e2427be8a9f81e
cereceres <jesus.edelcereceres@gmail.com>
2016-08-04 20:59:48 -0500

    the test for MLmodel is added

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 'use strict'
-process.env.MLDB_PATH = './db_ml'
+let assert = require('assert')
+process.env.MLDB_PATH = '../test/db_ml'
 describe('the test to machine learning', function () {
-
+  it('the test to MLModelId', function () {
+    let MLmodel = require('../lib/MLmodel')
+    assert(MLmodel.create)
+  })
 })
