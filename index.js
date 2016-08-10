3b6dd9cc185ae990ea07780d23366c64e1170ddb
cereceres <jesus.edelcereceres@gmail.com>
2016-08-10 16:21:52 -0500

    the machinelearning export all method avaiables

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 'use strict'
-const create = require('./lib/createMLModel')
+const create = require('./lib/createModel')
 const predict = require('./lib/makePrediction')
 const remove = require('./lib/delateModel')
+const createDataSourceFromS3 = require('./lib/createDataSourceFromS3')
+const delateEndPoint = require('./lib/delateEndPoint')
+const getDataSource = require('./lib/getDataSource')
+const getDataSourceComplete = require('./lib/getDataSourceComplete')
+const getEndPoint = require('./lib/getEndPoint')
+const getModelCompleted = require('./lib/getModelCompleted')
+const mlDataset = require('./lib/ml-dataset')
+const s3 = require('./lib/s3')
+const schema = require('./lib/schema')
+const updateMLModel = require('./lib/updateMLModel')
+const versioning = require('./lib/versioning')
+
 module.exports = class {
   constructor (options) {
     this.options = options
+    this.createDataSourceFromS3 = createDataSourceFromS3
+    this.delateEndPoint = delateEndPoint
+    this.getDataSource = getDataSource
+    this.getDataSourceComplete = getDataSourceComplete
+    this.getEndPoint = getEndPoint
+    this.getModelCompleted = getModelCompleted
+    this.mlDataset = mlDataset
+    this.s3 = s3
+    this.schema = schema
+    this.updateMLModel = updateMLModel
+    this.versioning = versioning
   }
   create (data, model, options) {
     options = options || {}
